691553d6a6a0dc1c19a4748d7dcec4c245bb955a
Patricio Rivera <patricio@patriciorivera.com>
2024-12-08 20:16:40 +0100

    node version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+/*   vite: {
+    server: {
+      host: process.env.HOST || 'localhost',
+      port: Number(process.env.PORT) || 3000
+    }
+  } */
 })
